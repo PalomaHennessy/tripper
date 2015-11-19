dedb83c2e38e6cdf20a9e94886aa43f05ba326c9
AraLawrence <ara.e.lawrence@gmail.com>
2015-11-19 14:15:30 -0800

    working on maps

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def get_js
+  	@goog_js = ENV['GOOGLE_JS']
+  end
+
   def current_user
   	@current_user ||=User.find_by_id(session[:user_id])
   end
