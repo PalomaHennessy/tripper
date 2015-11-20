860c757909a56b0b7036580bd789311503c6e8a3
AraLawrence <ara.e.lawrence@gmail.com>
2015-11-19 18:07:04 -0800

    merge issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-<<<<<<< HEAD
+
   before_action :current_user
 
   def is_authenticated?
@@ -11,13 +11,14 @@ class ApplicationController < ActionController::Base
       flash[:danger] = "Credentials Invalid!!"
       redirect_to login_path
     end
-=======
+  end
+
   def get_js
   	@goog_js = ENV['GOOGLE_JS']
->>>>>>> maps
   end
 
   def current_user
   	@current_user ||=User.find_by_id(session[:user_id])
   end
+
 end
